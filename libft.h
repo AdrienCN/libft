9b260b86a38fc0c05b7ebb40d20ca985835fe678
Adrien <adrien.consta@gmail.com>
2021-04-07 18:02:26 +0200

    auto-commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adconsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 14:36:23 by adconsta          #+#    #+#             */
-/*   Updated: 2021/01/29 09:28:41 by calao            ###   ########.fr       */
+/*   Updated: 2021/04/07 18:00:01 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
+int				ft_isbase(char *base, char c);
 int				ft_lstsize(t_list	*lst);
 int				ft_memcmp(const void *ptr, const void *ptr2, size_t num);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -61,6 +62,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *s, int c, size_t n);
+void			*ft_free_double_tab(char **tab);
 void			ft_bzero(void *s, size_t n);
 void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstadd_front(t_list **alst, t_list *new);
@@ -71,4 +73,5 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_putstr_fd(char *s, int fd);
+void			ft_print_double_tab_fd(char **tab, int fd);
 #endif
